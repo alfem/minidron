69d96d70309643a41cbc87985106a621e9226718
Alfonso E.M. <alfem@users.noreply.github.com>
2013-07-22 10:02:27 +0000

    reset audio in start screen

--- run-dron.sh
+++ run-dron.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+#reset alsa audio (only if no sound)
+#sudo alsactl init
+#sudo service alsa-utils stop && sudo alsa force-reload && sudo service alsa-utils start
+
 #set speaker level
 
 amixer sset Master Playback Volume  100%
 
+#start low-res video streaming
 mjpg_streamer -i "input_uvc.so -d /dev/video0 -y -r 320x240 -f 10" -o "output_http.so -w /home/brain/minidron/www -p 8080" --background
 
 #run the dron
