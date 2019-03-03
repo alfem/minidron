8e5888525dc5fb0d84401444bd3706cd4e7b3f6c
brain <brain@minidron.278.es>
2019-03-03 12:21:17 +0100

    Switched to festival

--- voice/voice.py
+++ voice/voice.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-import speechd
+import festival
 
 class Synthesizer:
     '''
     Speech Synthesizer for notifications (when no screen available) and user interface
+    switched from espeak to festival
     '''
     def __init__(self):
-
-      self.client=speechd.SSIPClient('minidron')
-
-      print self.client.list_output_modules()
-      print self.client.list_synthesis_voices()
-
-      self.client.set_output_module("espeak")
-      self.client.set_language("es")
-      self.client.set_synthesis_voice("spanish")
-      self.client.set_pitch(5)
-      self.client.set_rate(5)
-      self.client.set_punctuation(speechd.PunctuationMode.SOME)
-
+      return
 
     def say(self,text):
-      self.client.speak(text)
+      festival.say_text(text)
 
     def close(self):
-      self.client.close()
+      return
    
